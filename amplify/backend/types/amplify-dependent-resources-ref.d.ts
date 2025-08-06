d1e3ca83e719660885af185d855c7516e47e8aa2
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-06 14:32:25 +0530

    feat: add cognito part

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awscognitonextjs87749ed4": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+"use client"
+import { Amplify } from 'aws-amplify';
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import config from '../amplifyconfiguration.json';
+Amplify.configure(config);
 
-export default function Home() {
+export function App({ signOut, user }: WithAuthenticatorProps) {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <h1>Hello {user?.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
+
+export default withAuthenticator(App);
