ba97b1a6e6ce39fb0059bc7534bbf64caf51047e
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-06 14:40:10 +0530

    chor: remove shadcn button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from '../amplifyconfiguration.json';
-import { Button } from "@/components/ui/button"
+
 Amplify.configure(config);
 
 export function App({ signOut, user }: WithAuthenticatorProps) {
   return (
     <>
       <h1>Hello {user?.username}</h1>
-      <Button onClick={signOut}>Sign out</Button>
+      <button onClick={signOut}>Sign out</button>
     </>
   );
 }
